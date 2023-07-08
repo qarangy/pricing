21ebd8ceb46ae2df66fdb0843effb75abe48bb0e
qarangy <qarangy@gmail.com>
2023-07-08 11:29:19 +0500

    update plan prices payment period

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const elsPaymentPeriodRadio = document.querySelectorAll('[name="payment_period"]');
+const elsPricingPlanValue = document.querySelectorAll('.pricing-plan-value')
+
+elsPaymentPeriodRadio.forEach(function (elPaymentPeriodRadio) {
+  elPaymentPeriodRadio.addEventListener('change', function() {
+    const period = elPaymentPeriodRadio.value;
+
+
+    if (period === 'annually') {
+      elsPricingPlanValue.forEach(function (elPrice) {
+        elPrice.textContent = elPrice.closest('.pricing-plan').dataset.paymentAnnualy;
+      });
+    } else {
+      elsPricingPlanValue.forEach(function (elPrice) {
+        elPrice.textContent = elPrice.closest('.pricing-plan').dataset.paymentMonthly;
+      });
+    }
+  });
+});
